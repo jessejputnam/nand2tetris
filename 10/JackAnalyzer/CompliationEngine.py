4edb7c592ab79990c293c7a3ea4b7735d78329ab
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2024-07-11 20:58:25 -0400

    begin working on a var declaration

--- 10/JackAnalyzer/CompliationEngine.py
+++ 10/JackAnalyzer/CompliationEngine.py
@@ -3,9 +3,41 @@ from JackTokenizer import JackTokenizer
 
 class CompilationEngine:
     def __init__(self, input_path: str, output_path: str):
-        self.input = open(input_path, "r")
-        self.output = open(output_path, "w")
-        self.compile_class()
+        self.token = None
+        self.token_body = None
+        self.token_type = None
+        self.tokens = None
+        self.output = None
+
+        # self.compile_class()
+        with open(input_path, "r") as self.tokens:
+            with open(output_path, "w") as self.output:
+
+                for token in self.tokens:
+                    if token in ["<tokens>", "</tokens>"]:
+                        continue
+                    self.set_token(token)
+
+                    if self.token_type == "keyword":
+                        if self.token_body in ["var", "static"]:
+                            self.write("<classVarDec>")
+                            self.compile_class_var_dec()
+                            self.write("</classVarDec>")
+
+    def read_token(self):
+        self.set_token(self.tokens.readLine())
+
+        if self.token == "</tokens>":
+            return
+
+        if self.token_type == "tokens":
+            return self.read_token()
+
+        if self.token_type == "keyword":
+            if self.token_body in ["static", "var"]:
+                self.write("<classVarDec>")
+                self.compile_class_var_dec()
+                self.write("/<classVarDec>")
 
     def compile_class(self):
         # Compiles a complete class
@@ -13,7 +45,19 @@ class CompilationEngine:
 
     def compile_class_var_dec(self):
         # Compiles a static variable declaration or field declaration
-        pass
+        self.write(self.token)
+
+    #    <classVarDec>
+    #     <keyword> static </keyword>
+    #     <keyword> boolean </keyword>
+    #     <identifier> test </identifier>
+    #     <symbol> ; </symbol>
+    #   </classVarDec>
+
+    # <keyword> static </keyword>
+    # <keyword> boolean </keyword>
+    # <identifier> test </identifier>
+    # <symbol> ; </symbol>
 
     def compile_subroutine_dec(self):
         # Compiles a complete method, function, or constructor
@@ -24,7 +68,7 @@ class CompilationEngine:
         pass
 
     def compile_subroutine_body(self):
-        # COmpiles a subroutine's body
+        # Compiles a subroutine's body
         pass
 
     def compile_var_dec(self):
@@ -37,7 +81,8 @@ class CompilationEngine:
 
     def compile_let(self):
         # Compiles a let statement
-        pass
+        self.write("<letStatement>")
+        self.write(self.token)
 
     def compile_if(self):
         # Compiles an if statement, possibly with a trailing else clause
@@ -70,3 +115,11 @@ class CompilationEngine:
     def compile_expression_list(self):
         # Compiles a possibly empty comma-separated list of expressions
         pass
+
+    def write(self, token: str):
+        self.output.write(f"{token}\n")
+
+    def set_token(self, token: str):
+        self.token = token
+        self.token_type = token[1 : token.find(">")]
+        self.token_body = token[token.find(">") + 2 : token.rfind("</") - 1]
